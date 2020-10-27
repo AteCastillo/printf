765a2b4602f310f9cb4fa76e723f7604e1ea6c12
JulianArbini97 <1890@holbertonschool.com>
2020-10-27 18:38:56 +0000

    adv1

--- advfunctions1.c
+++ advfunctions1.c
@@ -3,29 +3,35 @@
 #include <stdarg.h>
 
 /**
- * print_bin - Converts unsigned int argument to binary
- * @bin: The decimal number to be converted
+ * print_binary - Converts unsigned int argument to binary
+ * @arg: The decimal number to be converted
  *
  * Return: The number of digits printed
  */
 
-int print_binary(va_list bin)
+int print_binary(va_list arg)
 {
-  unsigned int number, total = 0, i = 0;
-  int arr[100];
+unsigned int number, total = 0, i = 0;
+int arr[100];
 
-  number = va_arg(bin, int);
-
-      while (number > 0)
-	{
-	  arr[i] = number % 2;
-	  number /= 2;
-	  i++;
-	}
-  while (i--)
-    {
-      _put_char(arr[i] + '0');
-      total++;
-    }
-  return (total);
+if (number < 2)
+{
+_putchar(number + '0');
+count = 1;
 }
+else if (number >= 2)
+{
+number = va_arg(arg, int);
+while (number > 0)
+{
+arr[i] = number % 2;
+number /= 2;
+i++;
+}
+}
+while (i--)
+{
+_put_char(arr[i] + '0');
+total++;
+}
+return (total);
